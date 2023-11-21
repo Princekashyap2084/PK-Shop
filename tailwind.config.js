abe9f9fb4c8de89a1ecf40c24d3a90d8edafc095
Amit Kumar Raikwar <Gamingadda100k@gmail.com>
2023-11-21 14:23:08 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./components/**/*.{html,js}',
+  './pages/**/*.{html,js}',
+  './index.html',],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
